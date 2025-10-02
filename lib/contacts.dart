a059154931b705088239cdfe80ba2d03a4c24574
Talha-Ahsan5 <yousuftalha005@gmail.com>
2025-10-02 03:15:01 -0700

    changes made on contact

---
+++ lib/contacts.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Contacts extends StatelessWidget {
+  const Contacts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text('Contact page'),
+          )
+        ],
+      ),
+    );
+  }
+}
